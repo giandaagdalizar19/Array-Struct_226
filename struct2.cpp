c53d9e90a708932831dbc6eb96e9f2a3db8e9773
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-04-06 18:42:46 +0700

    mengisi data

--- struct2.cpp
+++ struct2.cpp
@@ -5,3 +5,25 @@ struct detailAlamat
     string desa;
     string kota;
 };
+struct Orang 
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
+int main () {
+    Orang mhs;
+    // Mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : " ;
+    cin >> mhs.nama;
+    cout << "desa : " ;
+    cin >> mhs.alamat.desa;
+    cout << "kota : " ;
+    cin >> mhs.alamat.kota;
+    cout << "Umur : " ;
+    cin >> mhs.umur;
+    cout << endl;
+
+}
