2a2b54e02063583060a8e0ed791bbb9465193c59
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-04-06 18:58:51 +0700

    menampilkan data

--- arraystruct.cpp
+++ arraystruct.cpp
@@ -34,5 +34,14 @@ int main() {
 
     }
 
+    for (int i = 0; i <= 2; i++)
 
+    {
+    // menampilkan data
+    cout << "Menampilkan Data" << endl;
+    cout << "Nama : " << mhs[i].nama << endl;
+    cout << "Desa : " << mhs[i].alamat.desa << endl;
+    cout << "Kota : " << mhs[i].alamat.kota << endl;
+    cout << "Umur : " << mhs[i].umur << endl;
+    }
 }
